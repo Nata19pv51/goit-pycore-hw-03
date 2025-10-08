da0e64c803f994476af42ccea8d214b6f20be3d0
Natalya <nidnatasha@gmail.com>
2025-10-08 22:39:05 +0300

    Add exception to the task 1

--- 1_task.py
+++ 1_task.py
@@ -4,15 +4,18 @@ def get_days_from_today(date):
     # Get today date
     date_today = datetime.today()
     
-    # Convert string into datetime according to the format %Y-%m-%d:
-    date = datetime.strptime(date, "%Y-%m-%d")
-    
-    total_seconds_in_day = 24 * 60 * 60
+    try:
+        # Convert string into datetime according to the format %Y-%m-%d:
+        date = datetime.strptime(date, "%Y-%m-%d")
+        total_seconds_in_day = 24 * 60 * 60
+        # Transform seconds to days 
+        delta = int((date_today - date).total_seconds() / total_seconds_in_day)
+        print("HELLO")        
+        return abs(delta)
+    except ValueError:
+        print("Oops!  That was no valid date format")
    
-    # Transform seconds to days 
-    delta = int((date_today - date).total_seconds() / total_seconds_in_day)
-    
-    return abs(delta)
+        
 
 # Ask the user to enter the date:
 data = input("Enter date in format YYYY-MM-DD: ")
