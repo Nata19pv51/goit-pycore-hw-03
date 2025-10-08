a86e8cad0ff588b269f0105ed723d2b45956bec1
Natalya <nidnatasha@gmail.com>
2025-10-08 17:18:35 +0300

    Add comments to the code

--- 1_task.py
+++ 1_task.py
@@ -1,15 +1,19 @@
 from datetime import datetime
 
 def get_days_from_today(date):
+    # Get today date
     date_today = datetime.today()
     
+    # Convert string into datetime according to the format %Y-%m-%d:
     date = datetime.strptime(date, "%Y-%m-%d")
     
     total_seconds_in_day = 24 * 60 * 60
-    
+   
+    # Transform seconds to days 
     delta = int((date_today - date).total_seconds() / total_seconds_in_day)
     
     return abs(delta)
-    
-data = input("Enter data in format YYYY-MM-DD: ")
+
+# Ask the user to enter the date:
+data = input("Enter date in format YYYY-MM-DD: ")
 print(get_days_from_today(data))
--- 2_task.py
+++ 2_task.py
@@ -1,13 +1,19 @@
 import random
 
 def get_numbers_ticket(min_num, max_num, quantity):
+    # Create an empty set:
     random_nums_set = set()
     
     while len(random_nums_set) != quantity:
+        # Use randrange() function to generate a number from min to max value, with step 1:
         random_num = random.randrange(min_num, max_num, 1)
+        
+        # Add random generated number to the set random_nums_set:
         random_nums_set.add(random_num)
-    
+        
+    # Sort the random_nums_set
     random_nums_set = sorted(random_nums_set)
+    
     return random_nums_set
 
 
